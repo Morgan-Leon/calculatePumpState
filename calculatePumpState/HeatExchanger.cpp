4aed8bada74f72d023ca4ad67dd40daebf2573aa
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2016-03-07 03:46:28 +0800

    添加溶液热交换器

---
+++ calculatePumpState/HeatExchanger.cpp
@@ -0,0 +1,9 @@
+//
+//  HeatExchanger.cpp
+//  calculatePumpState
+//
+//  Created by Morgan on 16/3/7.
+//  Copyright © 2016年 Morgan. All rights reserved.
+//
+
+#include "HeatExchanger.hpp"
---
+++ calculatePumpState/HeatExchanger.hpp
@@ -0,0 +1,59 @@
+//
+//  HeatExchanger.hpp
+//  calculatePumpState
+//
+//  Created by Morgan on 16/3/7.
+//  Copyright © 2016年 Morgan. All rights reserved.
+//
+
+#ifndef HeatExchanger_hpp
+#define HeatExchanger_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "Formular.hpp"
+
+using namespace std;
+
+/*
+ 溴化锂吸收式热泵的溶液热交换器（exchanger）
+ 降低发生器出口浓溶液温度，升高吸收器出口稀溶液温度，减轻吸收器负载以提高热能系数。
+ 
+ 已知：
+    稀溶液浓度XL和浓溶液浓度XH，且： XL = X7o = X2o, XH = X8o = X4o
+    T8o 与冷端温差通常控制在5˚C以上（5-25˚C），即∆T_h = 17（5至25之间）
+ 
+ 溶液热交换器:
+ 
+ 所求
+ 状态点    物质       位置           温度/˚C      压力/kPa       焓/(kJ/kg)   质量分数（LiBr）%
+ 7       稀溶液    发生器方向出口      T7o         ----            H7o          X7o
+ 8       浓溶液    吸收器方向出口      T8o         ----            H8o          X8o
+ 
+ ∆: /'delta/
+ 
+ */
+
+class heatExchanger{
+    
+private:
+    
+    double XL,XH;
+    double deltaT_h = 17;
+    double T7o,P7o,H7o,X7o;
+    double T8o,P8o,H8o,X8o;
+    
+public:
+    heatExchanger();
+    heatExchanger(double);
+    
+    void setDeltaT_h(double);
+    void setXL(double X2o);
+    void setXH(double X4o);
+    
+    void printHeatExchanger();
+
+    
+};
+#endif /* HeatExchanger_hpp */
